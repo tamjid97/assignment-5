7b947841950759f7c0e5e8f8de3d1391f5bcc537
epick32851 <tamjidhossain596@gmail.com>
2025-08-27 18:48:49 +0600

    added my js file

---
+++ script.js
@@ -0,0 +1,109 @@
+// 1
+document.getElementById("heartTipe").addEventListener("click",function (e){
+  e.preventDefault();
+  const added = 1
+  const heardnumbervalu = parseInt(document.getElementById("loveMain").innerText)
+  
+  const mainabba = added + heardnumbervalu 
+
+document.getElementById("loveMain").innerText=mainabba
+
+
+})
+
+// 2
+
+document.getElementById("heartTipe-2").addEventListener("click",function (e){
+  e.preventDefault();
+  const added = 1
+  const heardnumbervalu = parseInt(document.getElementById("loveMain").innerText)
+  
+  const mainabba = added + heardnumbervalu 
+
+document.getElementById("loveMain").innerText=mainabba
+
+})
+// 3
+document.getElementById("heartTipe-3").addEventListener("click",function (e){
+  e.preventDefault();
+  const added = 1
+  const heardnumbervalu = parseInt(document.getElementById("loveMain").innerText)
+  
+  const mainabba = added + heardnumbervalu 
+
+document.getElementById("loveMain").innerText=mainabba
+
+
+})
+// 4
+document.getElementById("heartTipe-4").addEventListener("click",function (e){
+  e.preventDefault();
+  const added = 1
+  const heardnumbervalu = parseInt(document.getElementById("loveMain").innerText)
+  
+  const mainabba = added + heardnumbervalu 
+
+document.getElementById("loveMain").innerText=mainabba
+
+
+})
+// 5
+document.getElementById("heartTipe-5").addEventListener("click",function (e){
+  e.preventDefault();
+  const added = 1
+  const heardnumbervalu = parseInt(document.getElementById("loveMain").innerText)
+  
+  const mainabba = added + heardnumbervalu 
+
+document.getElementById("loveMain").innerText=mainabba
+
+
+})
+// 6
+document.getElementById("heartTipe-6").addEventListener("click",function (e){
+  e.preventDefault();
+  const added = 1
+  const heardnumbervalu = parseInt(document.getElementById("loveMain").innerText)
+  
+  const mainabba = added + heardnumbervalu 
+
+document.getElementById("loveMain").innerText=mainabba
+
+
+})
+// 7
+document.getElementById("heartTipe-7").addEventListener("click",function (e){
+  e.preventDefault();
+  const added = 1
+  const heardnumbervalu = parseInt(document.getElementById("loveMain").innerText)
+  
+  const mainabba = added + heardnumbervalu 
+
+document.getElementById("loveMain").innerText=mainabba
+
+
+})
+// 8
+document.getElementById("heartTipe-8").addEventListener("click",function (e){
+  e.preventDefault();
+  const added = 1
+  const heardnumbervalu = parseInt(document.getElementById("loveMain").innerText)
+  
+  const mainabba = added + heardnumbervalu 
+
+document.getElementById("loveMain").innerText=mainabba
+
+
+})
+// 9
+document.getElementById("heartTipe-9").addEventListener("click",function (e){
+  e.preventDefault();
+  const added = 1
+  const heardnumbervalu = parseInt(document.getElementById("loveMain").innerText)
+  
+  const mainabba = added + heardnumbervalu 
+
+document.getElementById("loveMain").innerText=mainabba
+
+
+})
